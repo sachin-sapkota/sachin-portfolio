c696828ef02e31755ea1bad1c57c78ba2973bf0e
sachin <sachinsapkota773@gmail.com>
2023-04-27 10:13:04 +0545

    experience work

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,7 +1,48 @@
 import React from 'react';
 
+interface detailType {
+  position: string;
+  company: string;
+  companyLink: string;
+  time: string;
+  address: string;
+  work: string;
+}
+const Details = ({
+  position,
+  company,
+  companyLink,
+  time,
+  address,
+  work,
+}: detailType) => {
+  return (
+    <li>
+      <div>
+        <h3>
+          {position}&nbsp;<a href={companyLink}>@{company}</a>
+        </h3>
+        <span>
+          {time} | {address}
+        </span>
+        <p>{work}</p>
+      </div>
+    </li>
+  );
+};
 const Experience = () => {
-  return <div>Experience</div>;
+  return (
+    <div className="my-64">
+      <h2 className="font-bold text-8xl mb-32 w-full text-center">
+        Experience
+      </h2>
+      <div className="w-3/4 mx-auto relative">
+        <ul>
+          <li></li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Experience;
