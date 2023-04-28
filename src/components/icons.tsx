8d6afda77e8cd3e5a08a0072fd2776540102cc68
sachin <sachinsapkota773@gmail.com>
2023-04-28 12:12:17 +0545

    bug fixes

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -147,7 +147,7 @@ export const SunIcon = ({ className, ...rest }: classnameType) => (
         d="M7 6 C7 12.08 11.92 17 18 17 C18.53 17 19.05 16.96 19.56 16.89 C17.95 19.36 15.17 21 12 21 C7.03 21 3 16.97 3 12 C3 8.83 4.64 6.05 7.11 4.44 C7.04 4.95 7 5.47 7 6 Z"
         opacity="0"
       >
-        <set attributeName="opacity" begin="0.5s" to="1" />
+        {/* <set attributeName="opacity" begin="0.5s" to="1" /> */}
       </path>
     </g>
     <g fill="currentColor" fillOpacity="0">
@@ -273,7 +273,7 @@ export const SunIcon = ({ className, ...rest }: classnameType) => (
       fill="currentColor"
       mask="url(#lineMdSunnyFilledLoopToMoonFilledLoopTransition1)"
     >
-      <set attributeName="opacity" begin="0.5s" to="0" />
+      {/* <set attributeName="opacity" begin="0.5s" to="0" /> */}
       <animate
         fill="freeze"
         attributeName="r"
@@ -370,7 +370,7 @@ export const MoonIcon = ({ className, ...rest }: classnameType) => (
       strokeWidth="2"
     >
       <path d="M7 6 C7 12.08 11.92 17 18 17 C18.53 17 19.05 16.96 19.56 16.89 C17.95 19.36 15.17 21 12 21 C7.03 21 3 16.97 3 12 C3 8.83 4.64 6.05 7.11 4.44 C7.04 4.95 7 5.47 7 6 Z" />
-      <set attributeName="opacity" begin="0.6s" to="0" />
+      {/* <set attributeName="opacity" begin="0.6s" to="0" /> */}
     </g>
     <mask id="lineMdMoonFilledToSunnyFilledLoopTransition0">
       <circle cx="12" cy="12" r="12" fill="#fff" />
@@ -429,7 +429,7 @@ export const MoonIcon = ({ className, ...rest }: classnameType) => (
       mask="url(#lineMdMoonFilledToSunnyFilledLoopTransition0)"
       opacity="0"
     >
-      <set attributeName="opacity" begin="0.6s" to="1" />
+      {/* <set attributeName="opacity" begin="0.6s" to="1" /> */}
       <animate
         fill="freeze"
         attributeName="r"
